b3d3f57ecae8a228007c45649852e3a2c1025557
naike.drame-codelab <naike.drame.dev@gmail.com>
2025-01-12 22:46:21 +0100

    added exercices03 loops

---
+++ Exercices/Exercices03_Boucles/Exercice04_TableMultiplication/Program.cs
@@ -0,0 +1,19 @@
+﻿// A l’aide de boucles « for » afficher les 5 premières tables de multiplication en allant jusque « x20 »
+
+//for (int table = 1; table <= 5; table++)
+//{
+//    for (int multiplier = 1; multiplier <= 20; multiplier++)
+//    {
+//        Console.Write($"{table} X {multiplier} = {table * multiplier}");
+//        Console.Write("\t");
+//    }
+//    Console.Write("\n");
+//}
+
+// grâce au modulo, on peut éviter un double for :
+for (int i = 0; i < 100; i++)
+{
+    int table = (i / 20) + 1;
+    int multi = (i % 20) + 1;
+    Console.WriteLine($"{table} X {multi} = {table * multi}");
+}
---
+++ Exercices/Exercices03_Boucles/Exercice06_RacineCarree/Program.cs
@@ -0,0 +1,48 @@
+﻿//  Calculer la racine carré d’un nombre avec maximum 10 décimales (Math.Sqrt(x) ne peut être utilisée que pour vérifier la réponse)
+
+// f(x) = x² - 2 --> la courbe s'annule en -2 et 2
+// si a est une approximation de racine carré de 2, alors
+// (a² + 2) / 2a sera une meilleure approximation
+
+double a = 100;
+while (Math.Abs((a * a) - 2) > 0.0000000001)
+{
+    a = (a * a + 2) / (2 * a);
+    Console.WriteLine(a);
+}
+
+
+#region Algorithme de Newton-Raphson
+Console.Write("Entrez un nombre pour calculer sa racine carrée : ");
+double x = double.Parse(Console.ReadLine());
+
+if (x < 0)
+{
+    Console.WriteLine("La racine carrée d'un nombre négatif n'est pas définie pour les nombres réels.");
+    return;
+}
+
+double epsilon = 1e-10; // Précision jusqu'à 10 décimales
+double guess = x / 2;  // Estimation initiale
+
+// Algorithme de Newton-Raphson
+for (int i = 0; i < 100; i++) // Limite d'itérations pour éviter les boucles infinies
+{
+    double nextGuess = (guess + x / guess) / 2;
+
+    if (Math.Abs(nextGuess - guess) < epsilon) // Convergence atteinte
+    {
+        guess = nextGuess;
+        break;
+    }
+
+    guess = nextGuess;
+}
+
+Console.WriteLine($"La racine carrée calculée est : {guess:F10}");
+
+// Vérification avec Math.Sqrt
+double actualSqrt = Math.Sqrt(x);
+Console.WriteLine($"Vérification avec Math.Sqrt : {actualSqrt:F10}");
+#endregion
+
